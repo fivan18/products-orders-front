0b8d4689dc42e0f236c33396982ad0d343d08b8a
Ivan Guzman <ivanguzman@Ivans-MacBook-Air.local>
2021-07-07 09:13:22 -0500

    fix eslint errors

--- src/redux/foo/foo.types.js
+++ src/redux/foo/foo.types.js
@@ -1,5 +1,7 @@
 const FooActionTypes = {
-    ADD_FOOS: 'ADD_FOOS',
-  };
-  
-  export default FooActionTypes;
+  ADD_FOOS: 'ADD_FOOS',
+};
+
+export default FooActionTypes;
+
+export const ivancito = 'ivancito';
