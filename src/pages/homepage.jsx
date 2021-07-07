f3207a9c41193dbaea169265ad6ff382c988dd98
Ivan Guzman <ivanguzman@Ivans-MacBook-Air.local>
2021-07-07 11:58:10 -0500

    fix linter errors

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 
-const Homepage = () => {
-
-  return (
-    <div> Homepage </div>
-  );
-};
+const Homepage = () => (
+  <div> Homepage </div>
+);
 
 export default Homepage;
